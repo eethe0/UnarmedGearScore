5c28171d7236985cae9558d73660b4c427cf70e0
Etienne Orlhac <etienneorlhac@gmail.com>
2025-01-27 23:42:50 +0100

    first commit

---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace UnarmedGearScore;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static Harmony _harmony;
+    internal static ManualLogSource _log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+
+        // Harmony patching
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+        _log = Log;
+    }
+
+    public override bool Unload()
+    {
+        _harmony?.UnpatchSelf();
+        return true;
+    }
+
+}
