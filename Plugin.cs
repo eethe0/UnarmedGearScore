49012a0ee1c6e95dd5b18ba84bfaecd16f352c0a
Etienne Orlhac <etienneorlhac@gmail.com>
2025-01-28 12:14:39 +0100

    patch weapon level event instead of looping

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,4 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 
@@ -8,19 +7,19 @@ namespace UnarmedGearScore;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BasePlugin
 {
-    internal static Harmony _harmony;
-    internal static ManualLogSource _log;
+    Harmony _harmony;
+    internal static Plugin _instance;
 
     public override void Load()
     {
+        _instance = this;
+
         // Plugin startup logic
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
 
         // Harmony patching
         _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
-
-        _log = Log;
     }
 
     public override bool Unload()
---
+++ WeaponEventPatch.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using ProjectM;
+using ProjectM.Gameplay.Systems;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace UnarmedGearScore;
+
+[HarmonyPatch(typeof(WeaponLevelSystem_Spawn), nameof(WeaponLevelSystem_Spawn.OnUpdate))]
+static class WeaponEventPatch
+{
+    static World world = World.s_AllWorlds.ToArray().FirstOrDefault(world => world.Name == "Server");
+    static Dictionary<Entity, float> characterWeaponLevels = [];
+
+    static void Postfix(WeaponLevelSystem_Spawn __instance)
+    {
+        Update(__instance.__query_1111682356_0);
+    }
+
+    static void Update(EntityQuery query)
+    {
+        var entities = query.ToEntityArray(Allocator.Temp);
+
+        try
+        {
+            // Check each player's gear score and update it
+            foreach (var entity in entities)
+            {
+                if (!world.EntityManager.HasComponent<EntityOwner>(entity)) continue;
+                var characterEntity = world.EntityManager.GetComponentData<EntityOwner>(entity).Owner;
+
+                if (!world.EntityManager.Exists(characterEntity)) continue;
+                if (!world.EntityManager.HasComponent<Equipment>(characterEntity)) continue;
+                var equipment = world.EntityManager.GetComponentData<Equipment>(characterEntity);
+                var weaponLevel = equipment.WeaponLevel;
+
+                if (weaponLevel == 0f)
+                {
+                    // Player is unarmed
+                    // Set their weapon level to the previously recorded weapon level
+                    var previousWeaponLevel = characterWeaponLevels.GetValueOrDefault(characterEntity, 0f);
+
+                    equipment.WeaponLevel._Value = previousWeaponLevel;
+
+                    world.EntityManager.SetComponentData(characterEntity, equipment);
+                }
+                else
+                {
+                    // Player is armed
+                    // Save their current weapon level
+                    characterWeaponLevels[characterEntity] = weaponLevel;
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Plugin._instance.Log.LogError($"Error in UnarmedGearScore: {e}");
+        }
+        finally
+        {
+            entities.Dispose();
+        }
+    }
+}
